395e78f3a252a02ca8e7b81d80efb68a77a1e3fc
Guilherme Staciaki <gstaciaki99@gmail.com>
2024-04-19 11:45:13 -0300

    cretead files to retrieve tasks and one task by id

---
+++ public/tasks/show.php
@@ -0,0 +1,16 @@
+<?php
+
+$method = $_SERVER['REQUEST_METHOD'];
+$id = $_GET['id'];
+
+if ($method !== 'GET') {
+    http_response_code(405);
+    exit;
+}
+
+define('DB_PATH', '/var/www/database/tasks.txt');
+
+$tasks = file(DB_PATH, FILE_IGNORE_NEW_LINES);
+
+echo json_encode($tasks[$id])
+?>
