4f6e843cacadc06c3b73ecf5363c8810065e2732
guilhermestaciaki <guilherme.staciaki@fator3tech.com.br>
2024-07-03 12:11:35 -0300

    fix jwt class to verify signature

--- tests/Unit/Lib/Authentication/JWTTest.php
+++ tests/Unit/Lib/Authentication/JWTTest.php
@@ -75,4 +75,40 @@ class JWTTest extends TestCase
         $this->assertEquals($decodedJwt['payload'], $payload);
         $this->assertEquals($decodedJwt['header'], $header);
     }
+
+    public function test_should_throw_a_exception_on_decode_with_signature(): void
+    {
+        $data = [
+            'name' => 'root',
+            'email' => 'fulano@example.com',
+            'password' => '123456',
+            'password_confirmation' => '123456',
+            'created_at' => date_create()->format('Y-m-d H:i:s'),
+            'is_admin' => true
+        ];
+
+        $user = new User($data);
+        $user->save();
+
+
+        $header = [
+            "typ" => "JWT",
+            "alg" => "HS256"
+        ];
+        $payload = [
+            "sub" => $user->id,
+            "iat" => 1719888411,
+            "exp" => 1730688411,
+            "admin" => $user->isAdmin()
+        ];
+
+        $invalidJwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9'
+            . '.eyJzdWIiOjEsImlhdCI6MTcxOTg4ODQxMSwiZXhwIjoxNzMwNjg4NDExLCJhZG1pbiI6dHJ1ZX0'
+            . '.H247QIE0RSFyuEZXF2P-Fj_vj5QnB6bINqh8gAu52-1';
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Invalid JWT signature');
+
+        $decodedJwt = JWT::decode($invalidJwt);
+    }
 }
